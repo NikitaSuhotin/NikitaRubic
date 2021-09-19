a477a30393758d44db00c24cffebe5836f5b976c
NikitaSuhotin <nox.dregon@yandex.ru>
2021-09-19 17:52:31 +0300

    Начало сервака

---
+++ NikitaRubic/NikitaRubic/Controllers/BudgetContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NikitaRubic.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace NikitaRubic.Controllers
+{
+    public class BudgetContext : DbContext
+    {
+        public BudgetContext(DbContextOptions<BudgetContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Budget> Budgets { get; set; }
+    }
+}
